6d08fb8b0c5fb4d9102e6f90d11aa5c43cfcd79d
XavierGuichet <contact@xavier-guichet.fr>
2017-08-04 16:20:25 +0200

    Force decimal

--- public/partials/global-stats-tipster.php
+++ public/partials/global-stats-tipster.php
@@ -1,4 +1,4 @@
 <div class="statr">
-    <span class="le">Profit du mois :</span><span class="ri"><?php echo $gain_month; ?> unités</span><br/>
-    <span class="le">Profit total :</span><span class="ri"><?php echo $gain_global; ?> unités</span>
+    <span class="le">Profit du mois :</span><span class="ri"><?=sprintf('%.2f',$gain_month)?> unités</span><br/>
+    <span class="le">Profit total :</span><span class="ri"><?=sprintf('%.2f',$gain_global)?> unités</span>
 </div>
